0765def7b83c70d62be868ff88ec99deb49aa7e5
Francisco Thomas Chiressi <franciscotchiressi@gmail.com>
2026-05-30 21:40:53 -0300

    feat: add new file for export const with labels values

---
+++ base/scripts/labels.js
@@ -0,0 +1,23 @@
+export const TYPE_LABELS = {
+    income: 'Ingreso',
+    expense: 'Gasto',
+    savings: 'Meta de ahorro',
+}
+
+export const CATEGORY_LABELS = {
+    salary: 'Sueldo',
+    sales: 'Ventas',
+    food: 'Alimentos',
+    transport: 'Transporte',
+    services: 'Servicios',
+    entertainment: 'Entretenimiento',
+    amount: 'Reserva fija',
+    percent: 'Reserva porcentual',
+}
+
+
+export const SIGN_BY_TYPE = {
+    income:  '+',
+    expense: '−',
+    savings: '⊘',
+}
